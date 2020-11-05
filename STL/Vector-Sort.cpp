62dc0c403924ebec7c1adbdd96f0b7318362d73b
Aydin Simsek <43919074+aydinsimsek@users.noreply.github.com>
2020-11-06 00:44:30 +0300

    initial commit

---
+++ STL/Vector-Sort.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int N, x; 
+    vector <int> v; 
+    cin >> N; 
+    for(int i = 0; i < N; i++)
+    {
+        cin >> x;
+        v.push_back(x); 
+    } 
+    sort(v.begin(), v.end()); 
+    for(int i = 0; i < N; i++)
+    {
+        cout << v[i] << " "; 
+    }
+    return 0;
+}
