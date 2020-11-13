2e73cacd26229a35196aeee7140dc398b029c953
Aydin Simsek <43919074+aydinsimsek@users.noreply.github.com>
2020-11-13 19:30:12 +0300

    initial commit

---
+++ Strings/StringStream.cpp
@@ -0,0 +1,28 @@
+#include <sstream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> parseInts(string str) {
+    stringstream ss(str); 
+    int num;  
+    char ch; 
+    vector<int> v; 
+    while(ss >> num)
+    {
+        v.push_back(num);
+        ss >> ch; 
+    } 
+    return v; 
+}
+
+int main() {
+    string str;
+    cin >> str;
+    vector<int> integers = parseInts(str);
+    for(int i = 0; i < integers.size(); i++) {
+        cout << integers[i] << "\n";
+    }
+    
+    return 0;
+}
